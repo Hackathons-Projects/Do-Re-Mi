7bb438be7f34a371c7d6b9080f712127c5e2080a
Tanweer Ali <tanweeralii908@gmail.com>
2022-02-19 17:43:29 +0530

    Update server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
+const mental = require('./mental.json')
+const riddle = require('./riddles.json')
+const quote = require('./quotes.json')
 require('dotenv').config({ path: './env/.env' });
 app.use(express.json());
-app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/new", (req, res) => {
-    
+app.get("/quote", (req, res) => {
+    res.send(quote)
+});
+
+app.get("/mental", (req, res) => {
+    res.send(mental)
+});
+
+app.get("/riddle", (req, res) => {
+    res.send(riddle)
 });
 
 app.listen(PORT, () => console.log(`Server is Running at Port: ${PORT}`))
